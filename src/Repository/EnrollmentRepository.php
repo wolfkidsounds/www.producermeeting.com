6a5d40ca5c9d36d16d99d1ba4d6c0e63f1fe80a3
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-03-09 21:43:13 +0100

    Repositories

---
+++ src/Repository/EnrollmentRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Enrollment;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Enrollment>
+ *
+ * @method Enrollment|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Enrollment|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Enrollment[]    findAll()
+ * @method Enrollment[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EnrollmentRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Enrollment::class);
+    }
+
+    //    /**
+    //     * @return Enrollment[] Returns an array of Enrollment objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('e')
+    //            ->andWhere('e.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('e.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Enrollment
+    //    {
+    //        return $this->createQueryBuilder('e')
+    //            ->andWhere('e.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
