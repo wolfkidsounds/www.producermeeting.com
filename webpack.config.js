cec562f64bd1b5751d98077e888f3cb1ec6cca79
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-03-10 18:00:15 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,16 @@ Encore
      */
     .addEntry('app', './assets/app.js')
 
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+    })
+
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
+
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
