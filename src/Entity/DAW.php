4073ffbd967fed1ab8350ad69bc2e79e4c37a9a1
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-03-10 18:46:08 +0100

    Update DAW.php

--- src/Entity/DAW.php
+++ src/Entity/DAW.php
@@ -13,8 +13,58 @@ class DAW
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $URL = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Image = null;
+
+    public function __toString(): string
+    {
+        return $this->Name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getURL(): ?string
+    {
+        return $this->URL;
+    }
+
+    public function setURL(string $URL): static
+    {
+        $this->URL = $URL;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->Image;
+    }
+
+    public function setImage(string $Image): static
+    {
+        $this->Image = $Image;
+
+        return $this;
     }
 }
