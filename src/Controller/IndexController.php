a3b8afa950877be79064515e233ced62434add7a
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2024-03-14 15:59:52 +0100

    Update Controllers

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,33 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Config\PostStatus;
+use App\Repository\MeetingRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(MeetingRepository $meetingRepository): Response
     {
+        $posts = $meetingRepository->findBy(['PostStatus' => PostStatus::PUBLIC], ['Date' => 'DESC']);
         return $this->render('page/meeting/index.html.twig', [
-            'controller_name' => 'MeetingController',
+            'posts' => $posts,
+            'title' => 'Meetings',
         ]);
     }
 
     #[Route('/forum', name: 'app_forum')]
     public function forum(): Response
     {
-        return $this->render('meeting/index.html.twig', [
-            'controller_name' => 'MeetingController',
-        ]);
-    }
-
-    #[Route('/about', name: 'app_about')]
-    public function about(): Response
-    {
-        return $this->render('meeting/index.html.twig', [
-            'controller_name' => 'MeetingController',
-        ]);
+        return $this->redirect('https://forum.producermeeting.com/');
     }
 }
--- src/Controller/MeetingController.php
+++ src/Controller/MeetingController.php
@@ -2,17 +2,74 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Config\PostStatus;
+use App\Entity\Enrollment;
+use App\Form\EnrollmentFormType;
+use App\Repository\MeetingRepository;
+use App\Repository\EnrollmentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MeetingController extends AbstractController
 {
-    #[Route('/meeting/{slug}', name: 'app_meeting_single')]
-    public function meetingSingle(): Response
+    #[Route('/meeting/{slug}', name: 'app_meeting_single', methods: ['GET', 'POST'])]
+    public function meetingSingle(Request $request, string $slug, MeetingRepository $meetingRepository, EnrollmentRepository $enrollmentRepository, EntityManagerInterface $entityManager): Response
     {
+        $post = $meetingRepository->findOneBy(['Slug' => $slug, 'PostStatus' => PostStatus::PUBLIC]);
+        
+        $enrollment = new Enrollment();
+        $form = $this->createForm(EnrollmentFormType::class, $enrollment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $enrollment->setMeeting($post);
+
+            $existingEnrollment = $enrollmentRepository->findOneBy(['Mail' => $enrollment->getMail(), 'Meeting' => $post]);
+
+            if ($existingEnrollment) {
+
+                notyf()
+                    ->position('x', 'center')
+                    ->position('y', 'top')
+                    ->addWarning($enrollment->getMail() . ' existiert bereits, vermutlich bist du bereits angemeldet.');
+                
+                return $this->redirect($request->getUri());
+            }
+
+            $entityManager->persist($enrollment);
+            $entityManager->flush();
+
+            notyf()
+                ->position('x', 'center')
+                ->position('y', 'top')
+                ->addSuccess('Du wurdest mit ' . $enrollment->getMail() . 'angemeldet!')
+            ;
+            
+            return $this->redirect($request->getUri());
+        }
+
         return $this->render('page/meeting/single.html.twig', [
-            'controller_name' => 'MeetingController',
+            'form' => $form,
+            'post' => $post,
+            'title' => $post->getSpeaker()->getName(),
+        ]);
+    }
+
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/meeting/{slug}/enrollments', name: 'app_meeting_single_enrollments', methods: ['GET'])]
+    public function meetingSingleEnrollments(string $slug, MeetingRepository $meetingRepository, EnrollmentRepository $enrollmentRepository): Response
+    {
+        $post = $meetingRepository->findOneBy(['Slug' => $slug, 'PostStatus' => PostStatus::PUBLIC]);
+        $enrollments = $enrollmentRepository->findBy(['Meeting' => $post]);
+
+        return $this->render('page/meeting/single_enrollments.html.twig', [
+            'enrollments' => $enrollments,
+            'post' => $post,
+            'title' => $post->getSpeaker()->getName(),
         ]);
     }
 }
